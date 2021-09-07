7f207a80f874a9055b13150b4a2569b04ab4f9a2
Marcos Lamberti <lamberti.marcos049@gmail.com>
2021-09-07 17:22:23 -0300

    Criação do Projeto

---
+++ backend/src/main/java/com/devsuperior/vendasds/VendasdsApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.vendasds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasdsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/vendasds/VendasdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.vendasds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
