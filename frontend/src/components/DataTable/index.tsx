f4e4a33ef67a4eb17b03641393eb748445fb1c55
Marcos Lamberti <lamberti.marcos049@gmail.com>
2021-09-07 19:42:14 -0300

    Static Charts

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -32,7 +32,14 @@ const DataTable = () => {
                         <td>14</td>
                         <td>33</td>
                         <td>24010.00</td>
-                    </tr>                                        
+                    </tr> 
+                    <tr>
+                        <td>23/04/2021</td>
+                        <td>Jack Ben</td>
+                        <td>44</td>
+                        <td>35</td>
+                        <td>11055.00</td>
+                    </tr>                                                           
                 </tbody>
             </table>
         </div>
